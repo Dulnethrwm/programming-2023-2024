7e11131c921981f76bca39d8b6d2f8112bda9e10
Dulneth RWM <dulnethrwm2007@gmail.com>
2023-09-19 10:54:15 -0700

    Added some new notes and Python files

---
+++ Notes/1 - Natural Language Processing.md
@@ -0,0 +1,28 @@
+---
+tags:
+  - notes
+  - programming
+  - y2023
+---
+
+# Output
+We can display text and symbols using output.
+We use the print () function to display output.
+```python
+print()
+```
+
+# Comments
+Comments are pieces of texts that are *NOT* interpreted by Python.
+This means that the text is **ignored**
+We use the # symbol to make comments
+
+# [[Headers]]
+
+# Input
+We grab information from the user using input ().
+It will wit for the user to type something on and press Enter / Return.
+
+```python
+input()
+```
---
+++ quotegenerator.py
@@ -0,0 +1,18 @@
+# Quote Generator
+# Author: Dulneth
+# Date: 19 September 2023
+
+#  Output a quote from someone famous
+print("If you want to be the best, you have to do things that other people aren't willing to do - MP")
+
+# Print out another quote
+print("Life is like riding a bicycle. To keep your balance, you must keep moving - Albert Einstein")
+
+# Print out another, different quote
+print("Live as if you were to die tomorrow. Learn as if you were to live forever. - Mahatma Gandhi")
+
+# Ask the user a question
+input("What was your favourite quote? ")
+
+# Respond with something human would say
+print("Oh, that's my favourite too.")
