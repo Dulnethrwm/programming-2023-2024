3572bcd9bad9e24f33442a999fde2a2be743248a
Dulneth RWM <dulnethrwm2007@gmail.com>
2023-11-09 13:52:20 -0800

    made a new one

--- semester_evaluator.py
+++ semester_evaluator.py
@@ -8,19 +8,20 @@ import time
 print("Hey!")
 
 #  Ask the user about their courses
-courses = input("How many courses do you have this semester")
+courses = input("How many courses do you have this semester ")
 
-time.sleep(1)
+time.sleep(0.5)
 
 # Create a list if questions to ask
 questions = [
-    "How do you rate your course 1(out of 5)?",
-    "How do you rate course 2 (out of 5)?",
-    "How do you rate course 3(out of 5)?",
-    "How do you rate your 4th course (out of 5)?"
+    "How do you rate your course 1(out of 5)? ",
+    "How do you rate course 2 (out of 5)? ",
+    "How do you rate course 3(out of 5)? ",
+    "How do you rate your 4th course (out of 5)? "
 ]
 total_rating = 0
 
+total_r = [range(0, 3)]
 # Give the judge's score individually
 for answer in questions:
     time.sleep(1)
@@ -32,5 +33,12 @@ for answer in questions:
 
 # Calculate the average
 average_rating = total_rating / len(questions)
-print(f"Your averag rating is {average_rating}")
+print(f"Your average rating is {average_rating}")
 
+for average_r in total_r: 
+    if average_rating in range(0, 1):
+        print("Ouch")
+    elif average_rating in range(1,3):
+        print("Not a bad semester")
+    else:
+        print("Great!")
