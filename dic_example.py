88f782754aecc2724c6a33ba347357901e5ba51b
Dulneth RWM <dulnethrwm2007@gmail.com>
2024-01-23 12:07:41 -0800

    23.01.2024

---
+++ dic_example.py
@@ -0,0 +1,14 @@
+# Dictionary Example
+# Author: Dulneth
+# Date: 23 January 2024
+
+# creating a dictionary
+
+country_capitals = {
+  "Germany": "Berlin", 
+  "Canada": "Ottawa", 
+  "England": "London"
+}
+
+# printing the dictionary
+print(country_capitals)
